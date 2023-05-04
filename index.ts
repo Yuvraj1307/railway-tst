d63d5042504d17fe7c5c83391bc485f5fb40f74d
Yuvraj Maharshi <yogeshnda2018@gmail.com.com>
2023-05-04 22:47:30 +0530

    test 2

--- index.ts
+++ index.ts
@@ -1,9 +1,15 @@
 import express,{Request,Response} from "express";
-
+import connection from "./config/db";
 const app=express()
 app.get("/",(req:Request,res:Response):void=>{
     res.send("hello")
 })
-app.listen(4500,()=>{
-    console.log("connected to DB")
+app.listen(4500,async ()=>{
+    try{
+        await connection
+        console.log("connected to DB")
+    }catch(err){
+        console.log("can't connect")
+        console.log(err)
+    }
 })
